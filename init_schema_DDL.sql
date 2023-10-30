911b52c9a959e81c7a9a1a11db1457e71be51218
Rishi Agarwal <rishi.agl@outlook.com>
2023-10-30 18:20:49 +0530

    schema restructured

--- init_schema_DDL.sql
+++ init_schema_DDL.sql
@@ -17,8 +17,6 @@ CREATE TABLE IF NOT EXISTS company (
     upi_id TEXT NOT NULL,
     owner_email TEXT NOT NULL
 );
-INSERT INTO company(name, legal_name, address, city, state, pincode, gstn, phone_no, email, website, bank_name, account_no, ifsc_code, upi_id, owner_email) VALUES('My Choice Electronics', 'My Choice', 'Block-A, Sakchi', 'Jamshedpur', 'Jharkhand', '831001', '20AEYPA0067P1ZB', '9334638328', 'mychoice_jamshedpur@rediffmail.com', 'www.mychoiceelectronics.com', 'HDFC BANK', '74964976596957', 'HDFC108983', 'upiid@okhdfc', 'mychoiceeletronics01@gmail.com');
-INSERT INTO company(name, legal_name, address, city, state, pincode, gstn, phone_no, email, website, bank_name, account_no, ifsc_code, upi_id, owner_email) VALUES('Synergy Limited', 'Synergy Limited', 'Bistupur', 'Jamshedpur', 'Jharkhand', '831001', '20AHDVA0067P1ZB', '7979888545', 'synergylimited@gmail.com', 'www.synergylimited.com', 'HDFC BANK', '74964976596957', 'HDFC108983', 'upiid@okhdfc', 'rishi58235@gmail.com');
 
 DROP TABLE company_users;
 CREATE TABLE IF NOT EXISTS company_users (
@@ -29,8 +27,6 @@ CREATE TABLE IF NOT EXISTS company_users (
     FOREIGN KEY(company_id) REFERENCES company(id) ON DELETE RESTRICT,
     FOREIGN KEY(company_name) REFERENCES company(name) ON DELETE RESTRICT
 );
-INSERT INTO company_users(company_id, company_name, user_email) VALUES(1, 'My Choice', 'mychoiceelectronics01@gmail.com');
-INSERT INTO company_users(company_id, company_name, user_email) VALUES(2, 'Synergy Limited', 'rishi58235@gmail.com');
 
 DROP TABLE customer;
 CREATE TABLE IF NOT EXISTS customer(
@@ -40,9 +36,6 @@ CREATE TABLE IF NOT EXISTS customer(
    state TEXT NOT NULL,
    pincode TEXT
 );
-INSERT INTO customer(phone_no, name, address, state, pincode) VALUES ('7970460076', 'Rishi Agarwal', 'Hume Pipe Nirmal Nagar, Jamshedpur', 'Jharkhand', '831001');
-INSERT INTO customer(phone_no, name, address, state, pincode) VALUES ('9334638328', 'Rajesh Agarwal', 'Hume Pipe Nirmal Nagar, Jamshedpur', 'Jharkhand', '831009');
-INSERT INTO customer(phone_no, name, address, state, pincode) VALUES     ('8789738298', 'Akash Kumar', 'Hume Pipe Nirmal Nagar, Jamshedpur', 'west bengal', '821001');
 
 DROP TABLE product;
 CREATE TABLE IF NOT EXISTS product(
@@ -51,9 +44,6 @@ CREATE TABLE IF NOT EXISTS product(
     hsn TEXT NOT NULL,
     tax_rate INTEGER NOT NULL
 );
-INSERT INTO product(name, hsn, tax_rate) VALUES ('Bajaj Air Cooler', '84796000', 18);
-INSERT INTO product(name, hsn, tax_rate) VALUES ('Voltas Air Conditioner', '8415', 28);
-INSERT INTO product(name, hsn, tax_rate) VALUES ('Samsung Refridgerator', '82344523', 18);
 
 DROP TABLE transaction_metadata;
 CREATE TABLE IF NOT EXISTS transaction_metadata (
@@ -65,9 +55,6 @@ CREATE TABLE IF NOT EXISTS transaction_metadata (
     UNIQUE(type, company_name),
     FOREIGN KEY(company_name) REFERENCES company(name) ON DELETE RESTRICT
 );
-
-INSERT INTO transaction_metadata(type, company_name, prefix, last_inserted_id) VALUES('invoice', 'My Choice', 'inv', '1');
-INSERT INTO transaction_metadata(type, company_name, prefix, last_inserted_id) VALUES('invoice', 'Synergy Limited', 'inv', '0');
 
 DROP TABLE invoice;
 CREATE TABLE IF NOT EXISTS invoice (
@@ -91,7 +78,6 @@ CREATE TABLE IF NOT EXISTS invoice (
     UNIQUE(company_name, invoice_no),
     FOREIGN KEY(customer_phone_no) REFERENCES customer(phone_no) ON DELETE RESTRICT
 );
-INSERT INTO invoice(invoice_no, company_name, date_created, customer_phone_no, taxable_value, cgst, sgst, igst, amount, amount_paid, finance_name, finance_duration_in_months, dp, emi, narration, is_cancelled) VALUES('INV1', 'My Choice', '2023-09-16', '7970460076', 10000, 900, 900, 0, 11800, 10000, 'Bajaj Finance', 2800, 1000, 10, 'New Narration', 0);
 
 DROP TABLE invoice_product;
 CREATE TABLE IF NOT EXISTS invoice_product (
@@ -106,4 +92,3 @@ CREATE TABLE IF NOT EXISTS invoice_product (
     FOREIGN KEY(invoice_no) REFERENCES invoice(id) ON DELETE RESTRICT,
     FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE RESTRICT
 );
-INSERT INTO invoice_product(company_name, invoice_no, product_id, description, qty, rate) VALUES('My Choice', 'INV1', 1, 'a des', 2, 5000);
