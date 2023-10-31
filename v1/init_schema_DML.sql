85fb285bee670f2bb6c1f884cc92efdba3fb8f10
Rishi Agarwal <rishi.agl@outlook.com>
2023-11-01 01:07:04 +0530

    database versioning adopted

---
+++ v1/init_schema_DML.sql
@@ -0,0 +1,52 @@
+INSERT INTO company(name, legal_name, address, city, state, pincode, gstn, phone_no, email, website, bank_name, account_no, ifsc_code, upi_id, owner_email) VALUES('Demo Company', 'Demo Pvt Ltd.', 'Bolck-A, Floor-10, Tech Park, Salt Lake', 'Kolkata', 'West Bengal', '700010', '20AEYPA0899P1ZB', '9874567898', 'demopvtltd@gmail.com', 'www.demopvtltd.com', 'HDFC BANK', '74964976596957', 'HDFC108983', 'upiid@okhdfc', 'rishi58235@gmail.com');
+INSERT INTO company(name, legal_name, address, city, state, pincode, gstn, phone_no, email, website, bank_name, account_no, ifsc_code, upi_id, owner_email) VALUES('My Choice - Ammbagan', 'My Choice', '67, Birenu Trade Centre, Ammbagan', 'Jamshedpur', 'Jharkhand', '831001', '20AEYPA0067P1ZB', '7903186223', 'mychoicelectronics01@gmail.com', 'www.mychoiceelectronics.com', 'HDFC BANK', '10662000005544', 'HDFC0001066', '9334638328@okbizaxis', 'rishi58235@gmail.com');
+INSERT INTO company(name, legal_name, address, city, state, pincode, gstn, phone_no, email, website, bank_name, account_no, ifsc_code, upi_id, owner_email) VALUES('My Choice - Kalimati', 'My Choice', 'Block-1, Shop No-4 Kalimati Road, Sakchi', 'Jamshedpur', 'Jharkhand', '831001', '20AEYPA0067P1ZB', '9334638328', 'mychoicelectronics01@gmail.com', 'www.mychoiceelectronics.com', 'HDFC BANK', '10662000005544', 'HDFC0001066', '9334638328@okbizaxis', 'rishi58235@gmail.com');
+
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(1, 'Demo Company', 'rishi58235@gmail.com');
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(2, 'My Choice - Ammbagan', 'rishi58235@gmail.com');
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(3, 'My Choice - Kalimati', 'rishi58235@gmail.com');
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(3, 'My Choice - Kalimati', 'mychoicerajeshagarwal@gmail.com');
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(3, 'My Choice - Kalimati', 'agarwal791priya@gmail.com');
+INSERT INTO company_users(company_id, company_name, user_email) VALUES(3, 'My Choice - Kalimati', 'mychoiceelectronics01@gmail.com');
+
+INSERT INTO customer(phone_no, name, address, state, pincode) VALUES ('7970460076', 'Rishi Agarwal', 'Hume Pipe Nirmal Nagar, Jamshedpur', 'Jharkhand', '831001');
+
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Samsung Refrigerator', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('LG Refrigerator', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Voltas Refrigerator', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Panasonic Refrigerator', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Haier Refrigerator', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Samsung Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('LG Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Voltas Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Panasonic Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Haier Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('IFB Washine Machine', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Samsung Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('LG Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Voltas Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Panasonic Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Haier Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('IFB Microwave', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Samsung LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('LG LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('TCL LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('REALME LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Haier LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('OnePlus LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Xiaomi LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('MI LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Bajaj Air Cooler', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Symphony Air Cooler', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Telex Air Cooler', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Supreme Air Cooler', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Hindware Air Cooler', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Kelvinator LED', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Bajaj Mixer Grinder', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Cromptom Mixer Grinder', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Orient Mixer Grinder', '82344523', 18);
+INSERT INTO product(name, hsn, tax_rate) VALUES ('Havells Mixer Grinder', '82344523', 18);
+
+INSERT INTO transaction_metadata(type, company_name, prefix, last_inserted_id) VALUES('invoice', 'Demo Company', 'INV2223', '10');
+INSERT INTO transaction_metadata(type, company_name, prefix, last_inserted_id) VALUES('invoice', 'My Choice - Ammbagan', 'INV2223', '10');
+INSERT INTO transaction_metadata(type, company_name, prefix, last_inserted_id) VALUES('invoice', 'My Choice - Kalimati', 'INV2223', '10');
---
+++ v2/init_schema_DDL.sql
@@ -0,0 +1,99 @@
+DROP TABLE company;
+CREATE TABLE IF NOT EXISTS company (
+    name TEXT PRIMARY KEY,
+    legal_name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    pincode TEXT NOT NULL,
+    gstn  TEXT NOT NULL,
+    phone_no TEXT NOT NULL,
+    email TEXT NOT NULL,
+    website TEXT,
+    bank_name TEXT NOT NULL,
+    account_no TEXT NOT NULL,
+    ifsc_code TEXT NOT NULL,
+    upi_id TEXT NOT NULL,
+    owner_email TEXT NOT NULL
+);
+
+DROP TABLE employee;
+CREATE TABLE IF NOT EXISTS employee (
+    email TEXT PRIMARY KEY
+)
+
+DROP TABLE company_employee;
+CREATE TABLE IF NOT EXISTS company_users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT NOT NULL,
+    employee_email TEXT NOT NULL,
+    FOREIGN KEY(company_name) REFERENCES company(name) ON DELETE RESTRICT,
+    FOREIGN KEY(employee_email) REFERENCES employee(email) ON DELETE RESTRICT
+);
+
+DROP TABLE customer;
+CREATE TABLE IF NOT EXISTS customer(
+   phone_no TEXT PRIMARY KEY,
+   name TEXT NOT NULL,
+   address TEXT NOT NULL,
+   state TEXT NOT NULL,
+   pincode TEXT
+);
+
+DROP TABLE product;
+CREATE TABLE IF NOT EXISTS product(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    hsn TEXT NOT NULL,
+    tax_rate INTEGER NOT NULL
+);
+
+DROP TABLE transaction_metadata;
+CREATE TABLE IF NOT EXISTS transaction_metadata (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL,
+    company_name TEXT NOT NULL,
+    prefix TEXT NOT NULL,
+    last_inserted_id INTEGER,
+    UNIQUE(type, company_name),
+    FOREIGN KEY(company_name) REFERENCES company(name) ON DELETE RESTRICT
+);
+
+DROP TABLE invoice;
+CREATE TABLE IF NOT EXISTS invoice (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    invoice_no TEXT NOt NULL,
+    company_name TEXT NOT NULL,
+    date_created TEXT NOT NULL,
+    customer_phone_no INTEGER NOT NULL,
+    taxable_value INTEGER NOT NULL,
+    cgst INTEGER NOT NULL,
+    sgst INTEGER NOT NULL,
+    igst INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    amount_paid INTEGER NOT NULL,
+    finance_name TEXT,
+    finance_duration_in_months INTEGER,
+    dp INTEGER,
+    emi INTEGER,
+    narration TEXT,
+    employee_email TEXT NOT NULL,
+    is_cancelled INTEGER NOT NULL,
+    UNIQUE(company_name, invoice_no),
+    FOREIGN KEY(customer_phone_no) REFERENCES customer(phone_no) ON DELETE RESTRICT,
+    FOREIGN KEY(employee_email) REFERENCES employee(email) ON DELETE RESTRICT
+);
+
+DROP TABLE invoice_product;
+CREATE TABLE IF NOT EXISTS invoice_product (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT NOT NULL,
+    invoice_no TEXT NOT NULL,
+    product_id INTEGER NOT NULL,
+    description TEXT,
+    qty INTEGER NOT NULL,
+    rate INTEGER NOT NULL,
+    UNIQUE(company_name, invoice_no, product_id)
+    FOREIGN KEY(invoice_no) REFERENCES invoice(id) ON DELETE RESTRICT,
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE RESTRICT
+);
